0dda8e056cf370eed76b7b0c6227471c8ba98e3d
Priyanshi Gupta <priyanshigupta997@gmail.com>
2021-01-22 23:13:53 +0530

    Derivation of Logarthmic approach

--- no-of-digits.cpp
+++ no-of-digits.cpp
@@ -2,7 +2,7 @@ using namespace std;
 #include <bits/stdc++.h>
 
 //Iterative Solution
-// ! O(n)
+// ! O(digitsCount)
 int findNoOfDigits(int n)
 {
     int count = 0;
@@ -16,7 +16,7 @@ int findNoOfDigits(int n)
 }
 
 //Recusive Solution
-//! O(n)
+//! O(digitsCount)
 int recursive(int n)
 {
     if (n == 0)
@@ -29,6 +29,16 @@ int recursive(int n)
 
 //Logarithmic Solution
 //! O(1)
+//* Suppose the number of digits in the number N is K.
+//* Therefore, we can say that: 10^K-1 <= N < 10^K
+//* Applying base-10 logarithm to both sides in the above equation, we get:
+/*
+    K-1 <= log10(N) < K.
+
+    or, K - 1 + 1 <= log10(N) + 1 < K + 1
+    or, K <= log10(N) + 1 < K + 1 
+    K = floor(log10(N) + 1)
+*/
 int logarithmic(int n)
 {
     return floor(log10(n) + 1);
